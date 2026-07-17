97fb599331cbf439afbb59a180cbbdc84f7d24b4
Timothious Gill <timothious.gill@arhamsoft.com>
2026-07-17 18:16:38 +0500

    Convert a List Function into a Generator

--- Task3/Task3.py
+++ Task3/Task3.py
@@ -14,3 +14,23 @@ print(next(counter))
 print(next(counter))
 print(next(counter))
 print(next(counter))
+
+#---Task 2
+# ----- Convert a List Function into a Generator
+def simple_lst(lst):
+    empty = []
+    for item in lst:
+        empty.append(item**2)
+    return empty
+
+simple_lst_example = simple_lst
+print(simple_lst_example(([1,2,3,4])))
+def list_generator(lst):
+    for num in lst:
+        yield num ** 2
+
+
+gen_lst = list_generator([1, 2, 3, 4])
+print(gen_lst)
+for value in gen_lst:
+    print(value)
