fe98d2623c949139dc9eb8ed3a02a8ec99555ab5
Timothious Gill <timothious.gill@arhamsoft.com>
2026-07-17 18:01:54 +0500

    Tasl1: Lazy Counting generator

--- Task3/Task3.py
+++ Task3/Task3.py
@@ -1,4 +1,16 @@
 
-# Task 3
+# Day-3 Tasks
 
-#---
+#---Task 1
+# -----Lazy Counting Generator
+
+def count_generator(n):
+    for i in range(1, n+1):
+        yield i
+
+counter = count_generator(4)
+print(counter)
+print(next(counter))
+print(next(counter))
+print(next(counter))
+print(next(counter))
