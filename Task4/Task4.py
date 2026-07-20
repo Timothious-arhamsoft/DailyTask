eaabfc425ec2b97fd8871d82286f4d63a88c2b02
Timothious Gill <timothious.gill@arhamsoft.com>
2026-07-20 14:01:18 +0500

    Task1 Done

--- Task4/Task4.py
+++ Task4/Task4.py
@@ -11,7 +11,7 @@ class Car:
         self.color = color
     
     def __repr__(self):
-        return f"TThe name of the car is {self.model} with {self.color} color!"
+        return f"The name of the car is {self.model} with {self.color} color!"
     
     def __eq__(self, value):
         return self.model == value.model and self.color == value.color
@@ -28,3 +28,31 @@ print(repr(car3))
 # Test __eq__
 print(car1 == car2)   # True
 print(car1 == car3)   # False
+
+# print(car1 == "Toyota")   
+# --> Error:
+'''
+  File "/home/as/Downloads/Repos/DailyTask/Task4/Task4.py", line 31, in <module>
+    print(car1 == "Toyota")
+          ^^^^^^^^^^^^^^^^
+  File "/home/as/Downloads/Repos/DailyTask/Task4/Task4.py", line 17, in __eq__
+    return self.model == value.model and self.color == value.color
+                         ^^^^^^^^^^^
+AttributeError: 'str' object has no attribute 'model'
+
+'''
+
+# ---> DataClass
+from dataclasses import dataclass
+
+@dataclass
+class Car_v2:
+    model: str
+    color: str
+
+
+car1_v2 = Car_v2("BMW", "Black")
+car2_v2 = Car_v2("BMW", "Black")
+print(car1_v2)
+print(car1_v2 == car2_v2)
+
