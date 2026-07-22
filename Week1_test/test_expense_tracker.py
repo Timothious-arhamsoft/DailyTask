3f693b43451c6154952ae40dada1805e03e29f13
Timothious Gill <timothious.gill@arhamsoft.com>
2026-07-22 17:56:00 +0500

    Added the Test Cases

--- Week1_test/test_expense_tracker.py
+++ Week1_test/test_expense_tracker.py
@@ -1,4 +1,4 @@
-from expense_tracker import add_expense, Expense
+from expense_tracker import add_expense, iter_expenses, total_of
 
 def test_add_expense():
     result = add_expense(10, "food")
@@ -6,4 +6,30 @@ def test_add_expense():
     assert len(result) == 1
     assert result[0].amount == 10
     assert result[0].category == "food"
+
+
+
+def test_iter_expenses():
+    expenses = []
+    expenses = add_expense(40, "food", expenses)
+    expenses = add_expense(60, "transport", expenses)
+
+    total_expense = iter_expenses(expenses)
+
+    assert next(total_expense).amount == 40
+    assert next(total_expense).amount == 60
+
+
+def test_total_of():
+    expenses = []
+    expenses= add_expense(10, "food", expenses)
+    expenses = add_expense(50, "transport", expenses)
+    expenses = add_expense(60, "electricity", expenses)
+
+    total_expense = iter_expenses(expenses)
+    result = total_of(total_expense)
+    # print(expenses)
+    # print(result)
+    assert result == 120
+
 
