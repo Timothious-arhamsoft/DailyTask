86cf268b81b46a9185585d24114656fa6a4eaae7
Timothious Gill <timothious.gill@arhamsoft.com>
2026-07-28 17:45:47 +0500

    Added Task 1: Concurrency-vs-parallelism

---
+++ Task9/Task9.py
@@ -0,0 +1,21 @@
+# Task 1: Concurrency-vs-parallelism concept check, stated out loud
+
+
+'''
+Concurrency:
+Managing multiple tasks together by switching between them..
+
+Parallelism:
+Running multiple tasks simultaneously on multiple CPU cores.
+
+Threading:
+it uses Concurrency
+
+asyncio:
+it uses Concurrency
+
+Multiprocessing:
+it uses both Concurrency and Parallelism as it provides true parallelism.
+
+'''
+
