9fd03101b8d3923cb9a808ab134bb127d7868aba
Timothious Gill <timothious.gill@arhamsoft.com>
2026-08-06 13:21:43 +0500

    Added the Category model

--- Task13/app/models/models.py
+++ Task13/app/models/models.py
@@ -14,3 +14,12 @@ class User(Base):
     def __repr__(self):
         return f"<User(username='{self.username}', email='{self.email}')>"
     
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"<Category(name='{self.name}')>"
+    
