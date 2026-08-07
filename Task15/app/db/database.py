2a42cc720c9ba80472f415b9d136bd95dbce90ed
Timothious Gill <timothious.gill@arhamsoft.com>
2026-08-07 17:41:06 +0500

    Updated the Database file

--- Task15/app/db/database.py
+++ Task15/app/db/database.py
@@ -1,26 +1,23 @@
 import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-print("DATABASE_URL:", os.environ.get("DATABASE_URL"))
+
 SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
 
-connect_args = (
-    {"check_same_thread": False}
-    if SQLALCHEMY_DATABASE_URL.startswith("sqlite")
-    else {}
-)
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args=connect_args
+    SQLALCHEMY_DATABASE_URL
 )
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
+
 
 Base = declarative_base()
 
