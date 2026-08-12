f4b5b4c25075a758ad0f8b9a86409b1bfa07857f
Timothious Gill <timothious.gill@arhamsoft.com>
2026-08-12 17:48:15 +0500

    Cleanup the App files

--- Task15/app/main.py
+++ Task15/app/main.py
@@ -1,5 +1,7 @@
 # Checking Docker
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routers.notes import router as notes_router
 from app.routers.users import router as users_router
 from app.routers.auth import router as auth_router
@@ -8,6 +10,20 @@ from app.routers.admin import router as admin_router
 app = FastAPI(
     title="Notes API",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:3000",
+        "http://localhost:5174",
+        "*"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(notes_router)
